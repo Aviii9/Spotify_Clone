e3cb679fb1ac119d8e203572b50f74d268cbf044
Avishka <avishkak29@gmail.com>
2023-10-26 22:19:12 +0530

    Added Javascript file

--- script.js
+++ script.js
@@ -1,72 +1,109 @@
-console.log("Welcome To Spotify");
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('songs/song1.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
-let songItem = Array.from(document.getElementsByClassName('songItem'));
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
 let songs = [
-    { songName:"Despacito", filePath: "songs/song1.mp3", coverPath : "img/cover1.jpg" },
-    { songName:"Cover Me In Sunshine", filePath: "songs/song2.mp3", coverPath : "img/cover2.jpg" },
-    { songName:"Up", filePath: "songs/song3.mp3", coverPath : "img/cover3.jpg" },
-    { songName:"Fake Smile", filePath: "songs/song4.mp3", coverPath : "img/cover4.jpg" },
-    { songName:"Your Power", filePath: "songs/song5.mp3", coverPath : "img/cover5.jpg" },
-    { songName:"I can't fall in love", filePath: "songs/song6.mp3", coverPath : "img/cover6.jpg" }
+    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
-masterPlay.addEventListener('click', ()=>
-{
-    if(audioElement.paused || audioElement.currentTime<=0)
-    {
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
-        masterPlay.classList.remove('fa-play');
-        masterPlay.classList.add('fa-pause');
-        gif.style.opacity=1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
     }
-    else
-    {
+    else{
         audioElement.pause();
-        masterPlay.classList.remove('fa-pause');
-        masterPlay.classList.add('fa-play');
-        gif.style.opacity=0;
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
     }
 })
-audioElement.addEventListener('timeupdate', ()=>{
-    console.log("timeupdate");
-    progress = parseInt((audioElement.currentTime/audioElement.duration) * 100);
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
     myProgressBar.value = progress;
 })
 
-myProgressBar.addEventListener('change' ,()=>
-{
+myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
 })
-songItem.forEach((element,i)=>
-{
-    console.log(element,i);
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
 
-})
-const makeAllPlays=()=>
-{
-   
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach(()=>
-    {
-        e.target.classList.remove('fa-pause'),
-        e.target.classList.add('fa-play')
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
     })
 }
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>
-{
-    element.addEventListener('click',(e)=>
-    {
-        makeAllPlays();
-        e.target.classList.remove('fa-play'),
-        e.target.classList.add('fa-pause'),
-        audioElement.src='songs/song.mp3';
-        audioElement.currentTime=0;
-        audioElement.play();
-    })
 
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
 })
